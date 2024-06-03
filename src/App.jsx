2f659184cb05dae1e6af6b720586d665c5f1829f
Mahmud Sairoon <smbdmahmud1010@gmail.com>
2024-06-04 02:43:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import SubText from './components/SubText'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello React</h1>
+      <SubText />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SubText.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const SubText = () => {
+  return (
+    <div>
+        <h3>It is a subtext that goes under the title</h3>
+        <h4>Here is my first Homework</h4>
+    </div>
+  )
+}
+
+export default SubText
